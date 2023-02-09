fd81fbf240ca2ed07c3561794c1931aace03b257
Jack <jack@codegorilla.nl>
2023-02-09 11:07:50 +0100

    Starting to develop isGraduated

--- src/main/java/nl/codegorilla/sap/controller/IsGraduatedController.java
+++ src/main/java/nl/codegorilla/sap/controller/IsGraduatedController.java
@@ -1,7 +1,19 @@
 package nl.codegorilla.sap.controller;
 
+import nl.codegorilla.sap.service.IsGraduatedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/isgraduated")
 public class IsGraduatedController {
+
+
+    IsGraduatedService isGraduatedService;
+
+    @Autowired
+    public IsGraduatedController(IsGraduatedService isGraduatedService) {
+        this.isGraduatedService = isGraduatedService;
+    }
 }
--- src/main/java/nl/codegorilla/sap/repository/IsGraduatedRepository.java
+++ src/main/java/nl/codegorilla/sap/repository/IsGraduatedRepository.java
@@ -3,5 +3,5 @@ package nl.codegorilla.sap.repository;
 import nl.codegorilla.sap.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//public interface IsGraduatedRepository extends JpaRepository<Student, Long> {
-//}
+public interface IsGraduatedRepository extends JpaRepository<Student, Long> {
+}
