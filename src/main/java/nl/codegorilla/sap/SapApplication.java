53f942471aa8097189b7b567454b6ba39018a1eb
josgr <josgrooten@gmail.com>
2023-03-03 10:15:56 +0100

    Added console output for env variables

--- src/main/java/nl/codegorilla/sap/SapApplication.java
+++ src/main/java/nl/codegorilla/sap/SapApplication.java
@@ -1,13 +1,26 @@
 package nl.codegorilla.sap;
 
+import nl.codegorilla.sap.model.YAMLConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SapApplication {
+public class SapApplication implements CommandLineRunner {
+
+    @Autowired
+    private YAMLConfig yamlConfig;
 
     public static void main(String[] args) {
-        SpringApplication.run(SapApplication.class, args);
+        SpringApplication app = new SpringApplication(SapApplication.class);
+        app.run();
+    }
+
+    public void run(String... args) throws Exception {
+        System.out.println("URL: " + yamlConfig.getUrl());
+        System.out.println("User: " + yamlConfig.getUser());
+        System.out.println("Password: " + yamlConfig.getPassword());
     }
 
 }
---
+++ src/main/java/nl/codegorilla/sap/model/YAMLConfig.java
@@ -0,0 +1,38 @@
+package nl.codegorilla.sap.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class YAMLConfig {
+    private String url;
+    private String user;
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
