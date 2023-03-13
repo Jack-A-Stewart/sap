b1360b00ad126942665cb7e96dc4bdaf0f3d4e90
josgr <josgrooten@gmail.com>
2023-03-13 14:12:19 +0100

    Changed CsvHandler to work with Objects.

--- src/main/java/nl/codegorilla/sap/fileHandling/FileHandler.java
+++ src/main/java/nl/codegorilla/sap/fileHandling/FileHandler.java
@@ -1,15 +1,14 @@
 package nl.codegorilla.sap.fileHandling;
 
-import com.opencsv.exceptions.CsvException;
+import nl.codegorilla.sap.model.MailCourseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface FileHandler {
-    List<String[]> readCsvFile(MultipartFile file) throws IOException, CsvException;
+    List<MailCourseStatus> read(MultipartFile file);
 
-    void write(List<String[]> data);
+    String write(List<MailCourseStatus> list);
 
 }
 
